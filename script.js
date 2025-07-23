a3e0eff9c2ce14cd37d3fa8d794be1c675050b86
Bhavesh Suthar <officialbhaveshsuthar@gmail.com>
2025-07-23 16:45:55 +0530

    added new content

--- script.js
+++ script.js
@@ -23,20 +23,6 @@ $(document).ready(function () {
                     dots: true
                 }
             },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
         ]
     });
 });
